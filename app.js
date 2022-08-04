fa0c32c82950f730e3c00c3cba9b1a424de9e1e5
styrene <waghelaharsh56@gmail.com>
2022-08-04 20:08:18 +0530

    fix: bonus section complete

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const changeBtn = document.querySelector("#change-button");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const cashGivenSection = document.querySelector("#cash-given-section");
-const notesSection = document.querySelector(".notes-section");
+const notesSection = document.querySelector("#notes-section");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+nextBtn.addEventListener("click", () => {
+    if (billAmount.value > 0) {
+        cashGivenSection.style.display = "block";
+        errorMessage.innerText = "";
+    } else {
+        showMessage("Invalid Value");
+        cashGivenSection.style.display = "none";
+        notesSection.style.display = "none";
+    }
+});
 
 changeBtn.addEventListener("click", function validateInput() {
     hideMessage();
@@ -18,20 +29,10 @@ changeBtn.addEventListener("click", function validateInput() {
             calculateChange(returnAmount);
         } else {
             showMessage("Cash amount given should be greater than or equal to bill amount");
+            // notesSection.style.display = "none";
         }
     } else {
         showMessage("Bill amount should be greater thatn 0");
-    }
-});
-
-nextBtn.addEventListener("click", () => {
-    if (billAmount.value > 0) {
-        cashGivenSection.style.display = "block";
-        errorMessage.innerText = "";
-    } else {
-        showMessage("Invalid Value");
-        cashGivenSection.style.display = "none";
-        notesSection.style.display = "none";
     }
 });
 
